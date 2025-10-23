b45f92f1d6bbecd30b1cdd5b7d707ae1367b0558
Dongnyoung <leedo7182@naver.com>
2025-10-23 15:44:56 +0900

    feat: 참가자 유효성 검사클래스 의존성 주입

--- src/main/java/racingcar/domain/ParticipantRegistry.java
+++ src/main/java/racingcar/domain/ParticipantRegistry.java
@@ -1,15 +1,14 @@
 package racingcar.domain;
 
 public class ParticipantRegistry {
+    private final ParticipantValidator participantValidator;
     public ParticipantRegistry() {
-
+        participantValidator = new ParticipantValidator();
     }
     public StringBuilder[] decide(String[] participants,StringBuilder[] participantsStraight) {
         for (int i = 0; i < participants.length; i++) {
             //참가자 유효성 검사
-            if(participants[i].length()>5){
-                throw new IllegalArgumentException("자동차 이름은 5자이하여야함 ");
-            }
+            participantValidator.validate(participants[i]);
             StringBuilder str = new StringBuilder(participants[i]);
             participantsStraight[i] = str.append(" : "); //참가자 결정
         }
