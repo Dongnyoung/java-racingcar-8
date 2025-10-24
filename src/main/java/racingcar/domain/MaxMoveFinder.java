1a941c1ca3b38807fabe42f755cf440c345e75e0
Dongnyoung <leedo7182@naver.com>
2025-10-24 16:55:00 +0900

    feat: MaxMoveFinder클래스 작성, 가장많이 전진한 횟수체크 클래스

---
+++ src/main/java/racingcar/domain/MaxMoveFinder.java
@@ -0,0 +1,22 @@
+package racingcar.domain;
+
+import java.util.HashMap;
+
+public class MaxMoveFinder {
+    private int maxStraightCount;
+    private final HashMap<String,Integer> straightCountMap;
+    public MaxMoveFinder(HashMap<String,Integer> straightCountMap) {
+        this.straightCountMap= straightCountMap;
+    }
+    public void maxCount(String[] participants) {
+        int maxCount =0;
+        for(String part : participants) {
+            maxCount = Math.max(maxCount, straightCountMap.get(part));
+        }
+        maxStraightCount = maxCount;
+    }
+    public int getMaxStraightCount() {
+        return maxStraightCount;
+    }
+
+}
