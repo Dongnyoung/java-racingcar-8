b833d26650feec5cabe616ac734f16d6b1d3da37
Dongnyoung <leedo7182@naver.com>
2025-10-24 15:34:54 +0900

    feat: 전진개수 해쉬맵에 저장, 전진개수 체크
    전진개수 저장시 참가자의 이름이 필요한데 이곳을 String대신 StringBuilder를 이용하여 추출하였음. String은 불변 객체이기에 계속해서 객체를 만들어내기에 불필요한 메모리 사용이 발생.
    하지만, StringBuilder는 가변 버퍼를 사용하여 문자열을 효율적으로 이어붙일 수 있어서 메모리 측면에서 더 효율적이라고 생각함.

---
+++ src/main/java/racingcar/domain/MoveCounter.java
@@ -0,0 +1,40 @@
+package racingcar.domain;
+
+import java.util.HashMap;
+
+public class MoveCounter {
+    private final HashMap<String,Integer> straightCountMap;
+    public MoveCounter(String[] participants) {
+        this.straightCountMap = new HashMap<>();
+        //초기화
+        for(String part : participants) {
+            straightCountMap.put(part,0);
+        }
+    }
+    public void moveCheck(StringBuilder[] participantsStraight){
+        //직진 개수 체크
+        for(StringBuilder part : participantsStraight) {
+            String partStr = part.toString();
+            //System.out.println(partStr);
+            int straightCount =0;
+            for(char c : partStr.toCharArray()) {
+                if (c == '-') {
+                    straightCount++;
+                }
+
+            }
+            StringBuilder p = new StringBuilder();
+            for(char c : partStr.toCharArray()){
+                if(c==' '){
+                    break;
+                }
+                p.append(c);
+            }
+            String name = p.toString();
+            straightCountMap.put(name,straightCount);
+        }
+    }
+    public HashMap<String,Integer> getStraightCountMap() {
+        return new HashMap<>(straightCountMap); //그냥 해쉬맵을 반환하면 외부에서 수정 가능할 수 있음.
+    }
+}
