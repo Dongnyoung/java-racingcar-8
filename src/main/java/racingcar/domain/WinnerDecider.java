34100e299d304f8a3d5b666997b07804dbc9744e
Dongnyoung <leedo7182@naver.com>
2025-10-24 17:11:17 +0900

    feat: WinnerDecider우승자 결정 클래스

---
+++ src/main/java/racingcar/domain/WinnerDecider.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+import java.util.HashMap;
+
+public class WinnerDecider {
+    private int maxStraightCount;
+    private HashMap<String,Integer> straightCountMap;
+    private StringBuilder winnerStrBuilder;
+    public WinnerDecider(int maxStraightCount, HashMap<String,Integer> straightCountMap) {
+        this.maxStraightCount = maxStraightCount;
+        this.straightCountMap = straightCountMap;
+    }
+    public void decide(String[] participants){
+        //우승자 결정
+        winnerStrBuilder = new StringBuilder();
+        for(String part : participants) {
+            if(maxStraightCount ==straightCountMap.get(part)){
+                winnerStrBuilder.append(part);
+                winnerStrBuilder.append(", ");
+            }
+        }
+        winnerStrBuilder.delete(winnerStrBuilder.length()-2,winnerStrBuilder.length());
+    }
+    public StringBuilder getWinnerStrBuilder() {
+        return winnerStrBuilder;
+    }
+}
