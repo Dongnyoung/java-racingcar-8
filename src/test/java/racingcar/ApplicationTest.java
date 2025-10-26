4e82d80d5cd1133d4eaa39c41283fc3a7ca46c9b
Dongnyoung <leedo7182@naver.com>
2025-10-27 00:41:00 +0900

    test: 누적전진기능테스트, 최대전진횟수기능테스트, 동점다중우승자기능 테스트

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -2,6 +2,9 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import racingcar.service.MaxMoveFinder;
+
+import java.util.HashMap;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -15,22 +18,61 @@ class ApplicationTest extends NsTest {
     @Test
     void 기능_테스트() {
         assertRandomNumberInRangeTest(
-            () -> {
-                run("pobi,woni", "1");
-                assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
-            },
-            MOVING_FORWARD, STOP
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, STOP
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("pobi,javaji", "1"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("pobi,javaji", "1"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    @Test
+    void 누적_전진체크(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "3");
+                    assertThat(output()).contains("pobi : ---", "woni : --", "최종 우승자 : pobi");
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                MOVING_FORWARD,STOP,
+                MOVING_FORWARD,MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 최대전진_횟수체크() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("pobi", 3);
+        map.put("woni", 1);
+        map.put("jun", 2);
+
+        MaxMoveFinder finder = new MaxMoveFinder(map);
+        String[] participants = {"pobi", "woni", "jun"};
+
+        finder.maxCount(participants);
+
+        assertThat(finder.getMaxStraightCount()).isEqualTo(3);
+    }
+
+    @Test
+    void 동점_다중우승자(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "2");
+                    assertThat(output()).contains("pobi : -", "woni : -", "최종 우승자 : pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD,
+                STOP,STOP
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
