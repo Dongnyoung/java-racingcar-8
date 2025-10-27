cf3068cb6ba5472e4dceb7135d320f2a19a33c9f
Dongnyoung <leedo7182@naver.com>
2025-10-27 13:12:07 +0900

    fix: 인덴트 3이상으로 메소드로 분리하였음.

--- src/main/java/racingcar/service/Movement.java
+++ src/main/java/racingcar/service/Movement.java
@@ -4,28 +4,34 @@ import camp.nextstep.edu.missionutils.Randoms;
 import racingcar.view.io.ProgressView;
 
 public class Movement {
-    private StringBuilder[] moveStraight;
+    private final StringBuilder[] moveStraight;
     private final ProgressView progressView;
+
     public Movement(StringBuilder[] moveStraight) {
         this.moveStraight = moveStraight;
         this.progressView = new ProgressView();
     }
+
     public void move(int count) {
-        while(count > 0) {
-            count--;
-            int randomNumber = 0;
-            for(StringBuilder part : moveStraight) {
-                //랜덤 정수
-                randomNumber = Randoms.pickNumberInRange(0, 9);
-                //전진
-                if(randomNumber>=4){
-                    part.append("-");
-                }
-            }
-            //진행상황 출력
+        while (count-- > 0) {
+            moveParticipants();
             progressView.print(moveStraight);
         }
     }
+
+    private void moveParticipants() {
+        for (StringBuilder part : moveStraight) {
+            if (shouldMove()) {
+                part.append("-");
+            }
+        }
+    }
+
+    private boolean shouldMove() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= 4;
+    }
+
     public StringBuilder[] getMoveStraight() {
         return moveStraight;
     }
